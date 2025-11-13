7417124b8e2dd7a45a4efa33c07d7ddf2c872187
LinasD22 <linas.danusevicius@ktu.edu>
2025-11-14 00:50:25 +0200

    change sql dump

--- konfiguracija/audio_shop.sql
+++ konfiguracija/audio_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2025 at 10:14 PM
+-- Generation Time: Nov 13, 2025 at 11:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,7 +45,10 @@ INSERT INTO `krepšelio_prekė` (`krepšelio_id`, `prekės_id`, `kiekis`) VALUES
 (3, 1, 1),
 (4, 1, 1),
 (5, 2, 1),
-(5, 5, 1);
+(5, 5, 1),
+(8, 1, 1),
+(12, 1, 1),
+(14, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -64,13 +67,21 @@ CREATE TABLE `krepšelis` (
 --
 
 INSERT INTO `krepšelis` (`id`, `naudotojo_id`, `sukūrimo_data`) VALUES
-(1, 3, '2025-11-08 19:33:50'),
-(2, 4, '2025-11-09 19:33:50'),
+(1, NULL, '2025-11-08 19:33:50'),
+(2, NULL, '2025-11-09 19:33:50'),
 (3, NULL, '2025-11-10 19:33:50'),
 (4, NULL, '2025-11-12 19:53:09'),
 (5, 12, '2025-11-12 20:24:13'),
 (6, 12, '2025-11-12 20:24:37'),
-(7, 12, '2025-11-12 21:05:10');
+(7, 12, '2025-11-12 21:05:10'),
+(8, NULL, '2025-11-12 21:46:17'),
+(9, NULL, '2025-11-12 22:15:53'),
+(10, NULL, '2025-11-12 22:16:03'),
+(11, 12, '2025-11-12 23:10:09'),
+(12, 12, '2025-11-12 23:26:05'),
+(13, NULL, '2025-11-12 23:29:55'),
+(14, 12, '2025-11-12 23:41:05'),
+(15, 16, '2025-11-13 21:53:28');
 
 -- --------------------------------------------------------
 
@@ -92,12 +103,10 @@ CREATE TABLE `naudotojas` (
 --
 
 INSERT INTO `naudotojas` (`id`, `prisijungimo_vardas`, `paštas`, `slaptažodis`, `rolė`, `pinigai`) VALUES
-(1, 'vadybininkas1', 'vadyb@example.com', 'h', 'vadybininkas', 0.00),
-(2, 'buhalteris1', 'buh@example.com', 'hash123', 'buhalteris', 0.00),
-(3, 'jonas', 'jonas@example.com', 'hash123', 'vartotojas', 150.00),
-(4, 'ieva', 'ieva@example.com', 'hash123', 'vartotojas', 80.00),
-(5, 'petras', 'petras@example.com', 'hash123', 'vartotojas', 500.00),
-(12, 'linas', 'linas.danusevicius@ktu.edu', '$2y$10$fV7rC6SOWv8ZjdzVRQJh9eNiWWEKnucIpGl9GiV8KxcWP4vS012/m', 'vartotojas', 10000.00);
+(12, 'linas', 'linas.danusevicius@ktu.edu', '$2y$10$fV7rC6SOWv8ZjdzVRQJh9eNiWWEKnucIpGl9GiV8KxcWP4vS012/m', 'vartotojas', 8806.00),
+(14, 'v', 'v@ktu.edu', '$2y$10$jh5CDs8wNrIEwdfVw64RfeIZAO02ABLZRVGxftBtsIS/g//ooPfpu', 'vadybininkas', 0.00),
+(15, 'b', 'b@ktu.edu', '$2y$10$6LNGcu85tpXEoG351M9nSusPK0ZraIA/riVRaCtDNJ4BvVNGcpDdK', 'buhalteris', 0.00),
+(16, 'stud', 'stud@ktu.edu', '$2y$10$Hr5fwnCx36DJGSRmzVz03OFDe1nApryn1MWuNc3I2wysjZ7pees22', 'vartotojas', 0.00);
 
 -- --------------------------------------------------------
 
@@ -121,14 +130,14 @@ CREATE TABLE `prekė` (
 --
 
 INSERT INTO `prekė` (`id`, `pavadinimas`, `paskirtis`, `tipas`, `gamintojas`, `modelis`, `kaina`, `likutis`) VALUES
-(1, 'Ausinės HD560S', 'klausymui', 'ausinės', 'Sennheiser', 'HD560S', 199.00, 25),
-(2, 'Ausinės ATH-M50x', 'klausymui', 'ausinės', 'Audio-Technica', 'ATH-M50x', 149.00, 40),
+(1, 'Ausinės HD560S', 'klausymui', 'ausinės', 'Sennheiser', 'HD560S', 199.00, 22),
+(2, 'Ausinės ATH-M50x', 'klausymui', 'ausinės', 'Audio-Technica', 'ATH-M50x', 149.00, 38),
 (3, 'Garso interfeisas Scarlett 2i2', 'įrašymui', 'garso interfeisas', 'Focusrite', 'Scarlett 2i2 (4th Gen)', 189.00, 15),
 (4, 'Studijinis mikrofonas NT1', 'įrašymui', 'mikrofonas', 'RØDE', 'NT1 (2021)', 269.00, 10),
-(5, 'Stiprintuvas A-9110', 'klausymui', 'stiprintuvas', 'Onkyo', 'A-9110', 299.00, 8),
+(5, 'Stiprintuvas A-9110', 'klausymui', 'stiprintuvas', 'Onkyo', 'A-9110', 299.00, 7),
 (6, 'Skaitmeninis-analoginis keitiklis D50s', 'klausymui', 'DAC', 'Topping', 'D50s', 249.00, 12),
-(7, 'Kolonėlės R1280T', 'klausymui', 'kolonėlės', 'Edifier', 'R1280T', 129.00, 30),
-(8, 'Kabelis 3.5mm–RCA 1.5m', 'priedai', 'kabelis', 'Hama', '001', 9.99, 100);
+(7, 'Kolonėlės R1280T', 'klausymui', 'kolonėlės', 'Edifier', 'R1280T', 129.00, 29),
+(8, 'Kabelis 3.5mm–RCA 1.5m', 'priedai', 'kabelis', 'Hama', '001', 9.99, 99);
 
 -- --------------------------------------------------------
 
@@ -145,16 +154,6 @@ CREATE TABLE `užsakymas` (
   `rezervacijos_galiojimo_data` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_lithuanian_ci;
 
---
--- Dumping data for table `užsakymas`
---
-
-INSERT INTO `užsakymas` (`id`, `naudotojo_id`, `būsena`, `suma`, `sukūrimo_data`, `rezervacijos_galiojimo_data`) VALUES
-(1, 3, 'priimtas', 138.99, '2025-11-09 19:32:35', NULL),
-(2, 4, 'rezervuotos_prekės', 158.99, '2025-11-10 19:32:35', '2025-11-17 19:32:35'),
-(3, 5, 'įvykdytas', 548.00, '2025-11-05 19:32:35', NULL),
-(4, 12, 'pateiktas', 597.00, '2025-11-12 21:05:18', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -167,19 +166,6 @@ CREATE TABLE `užsakymo_prekė` (
   `kiekis` int(11) NOT NULL DEFAULT 1,
   `kaina` decimal(12,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_lithuanian_ci;
-
---
--- Dumping data for table `užsakymo_prekė`
---
-
-INSERT INTO `užsakymo_prekė` (`užsakymo_id`, `prekės_id`, `kiekis`, `kaina`) VALUES
-(1, 7, 1, 129.00),
-(1, 8, 1, 9.99),
-(2, 2, 1, 149.00),
-(2, 8, 1, 9.99),
-(3, 5, 1, 299.00),
-(3, 6, 1, 249.00),
-(4, 1, 3, 199.00);
 
 --
 -- Indexes for dumped tables
@@ -234,13 +220,13 @@ ALTER TABLE `užsakymo_prekė`
 -- AUTO_INCREMENT for table `krepšelis`
 --
 ALTER TABLE `krepšelis`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `naudotojas`
 --
 ALTER TABLE `naudotojas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `prekė`
@@ -252,7 +238,7 @@ ALTER TABLE `prekė`
 -- AUTO_INCREMENT for table `užsakymas`
 --
 ALTER TABLE `užsakymas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
